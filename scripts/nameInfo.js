6d79422002d78a9af0c8ba436babf01999ffe316
Ajmain Fayek <fayekajmain0@gmail.com>
2024-10-01 16:52:56 +0600

    first commit

---
+++ scripts/nameInfo.js
@@ -0,0 +1,41 @@
+// fetch("https://jsonplaceholder.typicode.com/users")
+//     .then(response => response.json())
+//     .then(data => data(data))
+
+const getUserData = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+    nameList(data);
+};
+
+window.addEventListener("load", () => {
+    getUserData();
+});
+
+const nameList = (data) => {
+    const name_area = document.getElementById("name_area");
+    for (let user of data) {
+        const li = document.createElement("li");
+        li.classList.add(
+            "py-2",
+            "list-none",
+            "cursor-pointer",
+            "font-semibold"
+        );
+        li.textContent = user.name;
+        li.addEventListener("click", () => {
+            showDetails(user);
+        });
+        name_area.appendChild(li);
+    }
+};
+
+const showDetails = (user) => {
+    const details_area = document.getElementById("details_area");
+    details_area.innerHTML = `<h2 class="font-bold text-lg">${user.name}</h2>
+                            <p>City: ${user.address.city || "N/A"}</p>    
+                            <p>Phone: ${user.phone || "N/A"}</p>    
+                            <p>Website: ${user.website || "N/A"}</p>    
+                            <p>Email: ${user.email || "N/A"}</p>
+                            `;
+};
